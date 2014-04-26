249a6922ff7dd0faa04befccbe51517c26f86683
Roman Sharf <pencilking2002@gmail.com>
2014-04-26 18:36:56 -0400

    LeanTween, Custom event handler and new test Scene
    I added the LeanTween plugin to try tweens. Had some trouble with it
    but its supposed to be very fast, especially on mobile.
    
    Tried implementing a custom event handler on the Markers
    
    Added a Tests scene where can do non AR tests

---
+++ AugmentedRC/Assets/RomanScripts/FadeIn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FadeIn : MonoBehaviour {
+	
+	public float duration = 1f;
+	public float transparent = 0f;
+	public float opaque = 1f;
+	
+	void Start ()
+	{
+	
+	}
+	void OnMouseDown ()
+	{
+		FadeObjectIn ();
+	}
+	
+	void FadeObjectIn ()
+	{
+		print ("Fading color in");
+		
+		// Fade GO in from 0 to 1, but it doesn't work?
+		LeanTween.alpha(gameObject, opaque, duration);
+	}
+	
+}
---
+++ AugmentedRC/Assets/RomanScripts/RCTrackableEventHandler.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Roman's event handler for tracking objects
+public class RCTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+	// Colors for fading in / out
+	private Color colorStart = new Color (1f, 1f, 1f, 0.0f);
+	private Color colorEnd = Color.white;
+	private float duration = 10.0f;
+	private bool canFadeIn = false;
+	
+	private Renderer[] rendererComponents;
+	
+	private TrackableBehaviour mTrackableBehaviour;
+	
+	void Start()
+	{
+		rendererComponents = GetComponentsInChildren<Renderer>(true);
+		
+//		foreach (Renderer rend in rendererComponents)
+//			rend.material.color = colorStart;
+		
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	void Update ()
+	{
+		if (canFadeIn == true)
+		{
+			rendererComponents = GetComponentsInChildren<Renderer>(true);
+			
+			foreach (Renderer rend in rendererComponents)
+			{	
+			
+			}
+		}
+	}
+	
+	// When the target's state changes, this function runs
+	public void OnTrackableStateChanged (TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+	{
+//		if (newStatus == TrackableBehaviour.Status.DETECTED)
+//		{
+//			print ("color is supposed to fade in");
+//			OnTrackingDetected ();
+//		}
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			print ("ROMAN: Object has been tracked. The status is:" + newStatus);
+			canFadeIn = true;
+			//OnTrackingFound();
+			//FadeIn ();
+		}
+		else
+		{
+			//OnTrackingLost();
+		}
+	}
+	
+	// Access all the game Objects attached to the Target
+	// and fade in the color
+	private void FadeIn ()
+	{
+		
+		rendererComponents = GetComponentsInChildren<Renderer>(true);
+		
+		foreach (Renderer rend in rendererComponents)
+		{	
+			print (rend.material.color);
+			for (float i = 0.0f; i < duration; i+= 0.01f)
+			{
+				print ("Fading color in");
+				rend.material.color = Color.Lerp (colorStart, colorEnd, i/duration);
+			}
+		}
+	}
+	
+	private void OnTrackingFound ()
+	{
+		Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		
+		// Enable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = true;
+		}
+		
+		// Enable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = true;
+		}
+		
+		Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+	}
+	
+	
+	private void OnTrackingLost()
+	{
+		rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		
+		// Disable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = false;
+		}
+		
+		// Disable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = false;
+		}
+		
+		Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+	}
+	
+}
